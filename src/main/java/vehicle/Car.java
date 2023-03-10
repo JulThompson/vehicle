10e19ed0553d1db051537645888f448dab14f589
Julia Thompson <jewelst2014@gmail.com>
2023-03-10 10:40:22 -0500

    Almost done with car

--- src/main/java/vehicle/Car.java
+++ src/main/java/vehicle/Car.java
@@ -1,57 +1,85 @@
 package vehicle;
 
+import java.util.List;
+
 public abstract class Car {
     String carMake;
     String carModel;
-    double startMileage;
+    double mileage;
     /** Creates a car with a starting mileage on the odometer.
     @throws IllegalArgumentException if startingMileage is negative*/
     public Car(String make, String model, double startingMileage) {
+        if (startingMileage < 0) {
+            throw new IllegalArgumentException("Cannot have a car with a negative mileage.");
+        }
         carMake = make;
         carModel = model;
-        if (startingMileage < 0) {
-            throw new IllegalArgumentException("Cannot have a car with negative mileage.");
-        }
-        startMileage = startingMileage;
+        mileage = startingMileage;
 
     }
 
     /** Starting mileage is 0. */
-    public Car(String make, String model)
+    public Car(String make, String model) {
+        carMake = make;
+        carModel = model;
+        mileage = 0;
+    }
 
     /** If able to drive the full given number of miles, returns true. If
     not, returns false.
     @throws IllegalArgumentException if miles is negative.*/
-    public boolean canDrive(double miles)
+    public boolean canDrive(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException("Cannot drive negative miles.");
+        }
+        if (miles < getRemainingRange()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /** Drives the full given number of miles.
     @throws IllegalArgumentException if miles is negative or if miles is
     too high given the current fuel. */
-    public abstract void drive(double miles)
+    public abstract void drive(double miles);
 
     /** Gives String representation of Car as
     "<make and model> (<mileage> mi)"
     Mileage should be rounded to 1 decimal place. If mileage is a whole
     number, ".0" should still display.
     */
-    public String toString()
+    public String toString() {
+        return "<" + carMake + carModel + ">" + " (<" + mileage + "> mi)";
+    }
 
     /** Returns how many miles have been driven so far (odometer). */
-    public double getMileage()
+    public double getMileage() {
+        return mileage;
+    }
 
     /** Returns the make of the car. */
-    public String getMake()
+    public String getMake() {
+        return carMake;
+    }
 
     /** Returns the model of the car. */
-    public String getModel()
+    public String getModel() {
+        return carModel;
+    }
 
     /** Returns how many more miles the car can currently go given the
     remaining fuel/energy reserves. */
-    public abstract double getRemainingRange()
+    public abstract double getRemainingRange();
 
     /** Adds mileage to the odometer.
      @throws IllegalArgumentException if miles is negative. */
-    protected void addMileage(double miles);
+    protected void addMileage(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException("Cannot drive negative miles.");
+        }
+        mileage += miles;
+    }
 
     /** The car attempts to drive, in order, each of the daily number of
     miles in the list milesEachDay. Once the car cannot drive one of the
@@ -60,6 +88,12 @@ public abstract class Car {
     @throws IllegalArgumentException if miles is negative for any of the
     attempted days. The exception check should occur prior to any driving
     is attempted. */
-    public int roadTrip(List<Double> milesEachDay)
-    abstract class GasPoweredCar ext
+    public int roadTrip(List<Double> milesEachDay) {
+        for (double miles: milesEachDay) {
+            if (miles < 0) {
+                throw new IllegalArgumentException("Cannot drive negative miles.");
+            }
+        }
+    }
 }
+
