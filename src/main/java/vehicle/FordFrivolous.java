4e44a646ad80541a964794f14929e5c05b4b204b
Julia Thompson <jewelst2014@gmail.com>
2023-03-13 10:30:16 -0400

    implemented interfaces for FordFrivolous

--- src/main/java/vehicle/FordFrivolous.java
+++ src/main/java/vehicle/FordFrivolous.java
@@ -1,13 +1,42 @@
 package vehicle;
 
-public class FordFrivolous extends GasPoweredCar {
+public class FordFrivolous extends GasPoweredCar implements SelfDriving, Flying {
     /** FordFrivolous has a gas tank of 20 gallons and an MPG of 23.6. */
     public FordFrivolous(double startingMileage) {
         // super("Ford", "Frivolous", 23.6, 20);
         super("Ford", "Frivolous", startingMileage, 23.6, 20);
     }
+
     /** Defaults mileage to 0. */
     public FordFrivolous() {
         super("Ford", "Frivolous", 0, 23.6, 20);
+    }
+
+    public void driveAutonomously(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException("Cannot drive negative miles.");
+        }
+        if (getRemainingRange() / 2 < miles) {
+            drive(getRemainingRange() / 2);
+        } else {
+            decreaseFuelLevel(miles * 2);
+            mileage += miles;
+        }
+    }
+
+
+    public boolean canFly(double miles) {
+        return canDrive(miles * 3);
+    }
+
+    public void fly(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException("Cannot drive negative miles.");
+        }
+        if (miles > getRemainingRange()) {
+            throw new IllegalArgumentException("Distance not within car's range.");
+        }
+        decreaseFuelLevel(miles * 3);
+        mileage += miles;
     }
 }
