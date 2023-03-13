75a5edd40dfd3da941e52970b1468f9789f78d85
Julia Thompson <jewelst2014@gmail.com>
2023-03-13 10:20:07 -0400

    SelfDriving

--- src/main/java/vehicle/SelfDriving.java
+++ src/main/java/vehicle/SelfDriving.java
@@ -6,8 +6,9 @@ public interface SelfDriving {
 }
 
 interface Flying {
-/** @throws IllegalArgumentException if miles is negative.*/
+    /** @throws IllegalArgumentException if miles is negative.*/
     public boolean canFly(double miles);
+    
     /** @throws IllegalArgumentException if miles is negative.
      @throws IllegalArgumentException if miles exceeds the remaining
     range of the car.
