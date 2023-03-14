7d38c588632c1a47213ce2ef853ae9707909f375
Julia Thompson <jewelst2014@gmail.com>
2023-03-14 10:15:07 -0400

    Fixed HondaAccordian

--- src/main/java/vehicle/HondaAccordian.java
+++ src/main/java/vehicle/HondaAccordian.java
@@ -1,17 +1,17 @@
 package vehicle;
 
 public class HondaAccordian extends GasPoweredCar {
-    int year;
+    private int year;
     /** modelYear specifies the year this car was made. Honda cares about
     that stuff. All Honda Accordian models have 14.5 gallon tanks and
     33.2 MPG. */
     public HondaAccordian(double startingMileage, int modelYear) {
-        super("Honda", "Accordian", startingMileage, 33.2);
+        super("Honda", "Accordian", startingMileage, 33.2, 14.5);
         year = modelYear;
     }
     /** Defaults mileage to 0. */
     public HondaAccordian(int year) {
-        super("Honda", "Accordian", 0, 33.2);
+        super("Honda", "Accordian", 0, 33.2, 14.5);
         this.year = year;
     }
     /** Prints out the model year, make and model, and mileage.
@@ -19,6 +19,6 @@ public class HondaAccordian extends GasPoweredCar {
     Coding tip: Write this method to re-use the behavior of the
     superclass toString. Don’t copy-and-paste the same code here. */
     public String toString()  {
-        return year + " " + getMake() + " " + getModel() + " (" + mileage + " mi) ";
+        return year + " " + getMake() + " " + getModel() + " (" + mileage + " mi)";
     }
 }
